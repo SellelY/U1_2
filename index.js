22e5988563597e5111a6fdd2a4612b2afe80a027
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-04-25 16:28:49 +0200

    loginPage()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const URL = "https://teaching.maumt.se/apis/access/";
+
+let user = null;
+let main = document.querySelector("main");
+
+if (window.localStorage.getItem("loginOrQuiz")) {
+    user = JSON.parse(window.localStorage.getItem("loginOrQuiz"));
+    attemptLogin();
+} else {
+    loginPage();
+}
---
+++ js/login_register.js
@@ -0,0 +1,56 @@
+function loginPage() {
+    main.innerHTML = `
+
+        <h2>Login</h2>
+        <form>
+
+            <h3>Username</h3>
+            <input type=text id=username>
+            <h3>Password</h3>
+            <input type=password id=password>
+
+            <p id=message><p>
+            <button type=submit>Login</button>
+
+            <button id=register><u>New to this? Register for free</u></button>
+
+        </form>
+
+    `;
+
+    let registerBtn = main.querySelector("#register");
+    registerBtn.addEventListener("click", registerPage);
+
+    let loginForm = main.querySelector("form");
+    loginForm.addEventListener("submit", async function(event) {
+
+        event.preventDefault();
+        let message = main.querySelector("#message");
+
+        try {
+
+            let response = await fetch(URL, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    username: this.elements.username.value,
+                    password: this.elements.password.value,
+                }),
+            });
+
+            let data = await response.json();
+            
+            if (!response.ok) {
+                connectFeedback(response.status);
+                message.innerHTML = `Wrong username or password.`;
+            } else {
+                window.localStorage.setItem("loginOrQuiz", JSON.stringify(data));
+                user = data;
+                quizPage();
+            }
+        } catch (error) {
+            message.textContent = `Error: ${error.message}`;
+        }
+    });
+}
+loginPage();
